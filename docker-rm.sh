58ad720f3582cb13c217940d1ae3745b0d44dfae
Nicolas Bermudez <ngbermudez@outlook.com>
2023-10-12 23:35:37 -0300

    add dockerfile

---
+++ docker-rm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker rm -fv postgresql_service
+
+docker rm -fv pgadmin_service
+
+docker ps
---
+++ docker-up.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose --env-file .docker.env -f ./docker-compose.yml up -d
